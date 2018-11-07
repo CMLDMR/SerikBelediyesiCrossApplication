88e9aadbd87b9aa74bfc94a9d8b457842fcb5add
Cemal Demir <CMLDMR@users.noreply.github.com>
2018-11-07 13:58:24 +0300

    Geliştirme: Utility Global Olarak Tanımlandı.

--- main.cpp
+++ main.cpp
@@ -5,10 +5,13 @@
 #else
 #endif
 
+#include <QtQml/QQmlContext>
+
 #include <QIcon>
 
 #include "qmlmongodb.h"
 #include "utility.h"
+#include "../url.h" // Remove this line and put on your own database in QString url and QString dbname
 
 int main(int argc, char *argv[])
 {
@@ -18,32 +21,15 @@ int main(int argc, char *argv[])
 
     app.setWindowIcon(QIcon(":/img/img/3D_Logox48.png"));
 
-    qmlRegisterType<Utility>("Utility", 0, 7, "Utility");
-
-
-    QFile file("../DBoptions");
-
-    QString url;
-    QString dbname;
-    if( file.open( QIODevice::ReadOnly ) )
-    {
-        QJsonObject info = QJsonDocument::fromBinaryData(file.readAll()).object();
-        url = info["url"].toString();
-        dbname = info["dbname"].toString();
-        file.close();
-        file.flush();
-    }else{
-        qDebug() << "file Can not Read for DB options";
-        return -1;
-    }
-
-        qDebug() << url;
+//    qmlRegisterType<Utility>("Utility", 0, 7, "Utility");
 
     QMLMongoDB::instance(url,dbname);
 
     QQmlApplicationEngine engine;
 
+    engine.rootContext()->setContextProperty("db",new QMLMongoDB());
 
+    engine.rootContext()->setContextProperty("Utility",new Utility());
 
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
     if (engine.rootObjects().isEmpty())
