24a844ede5849f65636b0f495fc8fbb5c58f000b
Cemal Demir <CMLDMR@users.noreply.github.com>
2018-11-12 15:08:04 +0300

    Personel Class Tanımlandı.

--- cppsrc/personel.cpp
+++ cppsrc/personel.cpp
@@ -1,6 +1,43 @@
 #include "personel.h"
 
-Personel::Personel(QObject *parent) : QObject(parent)
+Personel::Personel(QMongoDB *_db, QObject *parent) : QObject(parent) , db( _db)
 {
 
+    mPersonelDocument = std::make_unique<QBSON>();
+
+
 }
+
+Personel::~Personel()
+{
+
+    mPersonelDocument.release();
+}
+
+bool Personel::login(const QString &tel, const QString &password)
+{
+
+    QBSON filter;
+
+    filter.append("telefon",tel);
+
+    filter.append("password",password);
+
+    auto var = this->db->find_one("Personel",filter);
+
+    mPersonelDocument->append(var.getMaplist());
+
+
+    std::cout << mPersonelDocument.get()->tojson().c_str() << std::endl;
+
+
+    if( var.isEmpty() )
+    {
+        return false;
+    }else{
+        return true;
+    }
+
+
+}
+
--- cppsrc/personel.h
+++ cppsrc/personel.h
@@ -4,15 +4,41 @@
 #include <QtCore/QObject>
 #include <QtCore/qglobal.h>
 
+
+#include "qbson.h"
+#include "qmongodb.h"
+#include "qelement.h"
+#include "qerror.h"
+#include "qmongodb.h"
+
+#include "qmlbson.h"
+
+#include <iostream>
+#include <memory.h>
+
+
 class Personel : public QObject
 {
     Q_OBJECT
+//    Q_PROPERTY()
 public:
-    explicit Personel(QObject *parent = nullptr);
+    explicit Personel(QMongoDB* _db , QObject *parent = nullptr);
+    virtual ~Personel();
+
+
+    Q_INVOKABLE bool login(const QString& tel , const QString& password);
+
 
 signals:
 
 public slots:
+
+
+private:
+    std::unique_ptr<QBSON> mPersonelDocument;
+
+    QMongoDB* db;
+
 };
 
 #endif // PERSONEL_H
