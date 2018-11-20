eb91e30640c8005af084a0bb22b1595060abc425
Cemal Demir <CMLDMR@users.noreply.github.com>
2018-11-20 18:30:23 +0300

    Kullanıcı Girişi Tanımladı.

--- cppsrc/user.cpp
+++ cppsrc/user.cpp
@@ -1,6 +1,48 @@
 #include "user.h"
 
-User::User(QObject *parent) : QObject(parent)
+User::User(QMongoDB *_db, QObject *parent) : QObject(parent) , db(_db)
+{
+    mUserDocument = std::make_unique<QBSON>();
+
+    this->setLogined(false);
+}
+
+bool User::login(const QString &telefon, const QString &sifre)
 {
 
+    QBSON filter;
+
+    filter.append("Cep Telefonu",telefon);
+
+    filter.append("password",sifre);
+
+
+    auto val = this->db->find_one("TC",filter);
+
+    if( val.isEmpty() )
+    {
+        return false;
+    }else{
+        this->setLogined(true);
+        return true;
+    }
+
+
+}
+
+void User::logout()
+{
+    mUserDocument.get()->clear();
+    this->setLogined(false);
+}
+
+bool User::logined() const
+{
+    return mLogined;
+}
+
+void User::setLogined(bool logined)
+{
+    mLogined = logined;
+    emit loginedChanged();
 }
--- cppsrc/user.h
+++ cppsrc/user.h
@@ -4,15 +4,64 @@
 #include <QtCore/QObject>
 #include <QtCore/qglobal.h>
 
+
+#include "qbson.h"
+#include "qmongodb.h"
+#include "qelement.h"
+#include "qerror.h"
+#include "qmongodb.h"
+
+#include "qmlbson.h"
+
+#include <iostream>
+#include <memory.h>
+
 class User : public QObject
 {
-Q_OBJECT
+    Q_OBJECT
+    Q_PROPERTY(bool logined READ logined WRITE setLogined NOTIFY loginedChanged)
 public:
-explicit User(QObject *parent = nullptr);
+    explicit User(QMongoDB* _db , QObject *parent = nullptr);
+
+
+    ///
+    /// \brief login
+    /// \param telefon
+    /// \param sifre
+    /// \return
+    /// Telefon Numarası ve Şifre ile Veritabanını Kontrol et
+    /// Kullanıcı var is true yoksa false döndürür.
+    Q_INVOKABLE bool login( const QString& telefon , const QString& sifre);
+
+    ///
+    /// \brief logout
+    /// Kullanıcı Çıkış Yapar. mUserDocument içeriğini Temizler
+    Q_INVOKABLE void logout();
+
+    bool logined() const;
+    void setLogined(bool logined);
 
 signals:
-
+    void loginedChanged();
 public slots:
+
+
+private:
+
+    ///
+    /// \brief db
+    /// Veri Tabanı Bağlantısı
+    QMongoDB* db;
+
+    ///
+    /// \brief mUserDocument
+    /// Kullanıcı Bilgileri
+    std::unique_ptr<QBSON> mUserDocument;
+
+    ///
+    /// \brief mLogined
+    /// Kullanıcı Giriş Yapıldığına Dair Bilgi
+    bool mLogined;
 };
 
 #endif // USER_H
